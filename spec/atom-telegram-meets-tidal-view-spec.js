8952e6e5c08d30cf995d23a6293ae774959de164
Diego Dorado <diegodorado@gmail.com>
2019-03-16 21:17:37 -0300

    first commit

---
+++ spec/atom-telegram-meets-tidal-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTelegramMeetsTidalView from '../lib/atom-telegram-meets-tidal-view';
+
+describe('AtomTelegramMeetsTidalView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
